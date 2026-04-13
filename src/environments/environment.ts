353eb84e3fb449873cf7a13e57362ffd2c35384b
Henry Zhu <hzhu@onmed.com>
2026-04-13 15:18:53 -0400

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  mapboxToken: 'YOUR_MAPBOX_TOKEN_HERE'
+  mapboxToken: 'pk.eyJ1IjoiYmx1bmRlcm93bDIwMTgiLCJhIjoiY21hc3p0ZWd1MHNsaTJtb2dqcjg2MWVtZCJ9.0yXrCiAV3gM81pEXk-kDnw'
 };
